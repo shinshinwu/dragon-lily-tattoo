96f0cc1b73a8b0975afae495346283541cfd90b4
Anna Wu <shinshinwu@gmail.com>
2015-01-03 18:42:06 -0800

    Saving first rough copy with gallery and contact incomplete

---
+++ app/controllers/index.rb
@@ -0,0 +1,39 @@
+get "/" do
+  erb :index
+end
+
+get "/location" do
+  erb :location
+end
+
+get "/faq" do
+  erb :faq
+end
+
+get "/galleries" do
+  erb :galleries
+end
+
+get "/galleries/lily" do
+  erb :lily
+end
+
+get "/galleries/doug" do
+  erb :doug
+end
+
+get "/galleries/christina" do
+  erb :christina
+end
+
+get "/galleries/rob" do
+  erb :rob
+end
+
+get "/contact" do
+  erb :contact
+end
+
+get "/blog" do
+  erb :blog
+end
